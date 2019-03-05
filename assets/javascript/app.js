5177f4fd657d69c217d1a2557029a0b2e1fdbc1b
cganzon <crganzon@gmail.com>
2019-03-04 20:40:53 -0800

    added new button functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,35 @@
-var topics = ["pizza", "hot dogs", "hamburgers"];
+var topics = ["pizza", "hot dogs", "hamburgers", "chicken", "kbbq"];
 
 // Dynamically displaying buttons on page
-for (var i = 0; i < topics.length; i++) {
-    var button = $("<button>").text(topics[i]);
-    button.attr("class", "button");
-    $("#buttons").append(button);
-}
+function makeButtons() {
+    for (var i = 0; i < topics.length; i++) {
+        var button = $("<button>").text(topics[i]);
+        button.attr("class", "button");
+        $("#buttons").append(button);
+    };
+};
+// Calling function on page load to generate initial topics
+makeButtons();
+
+// Adding new buttons from user input
+$(document).on("click", "#addButton", function (event) {
+    // Disables the ability for the add button to refresh the page
+    event.preventDefault();
+    // Clears the buttons div in order to make new buttons with new input added
+    $("#buttons").empty();
+    // Getting user's text from the input box and making a new button
+    var newButton = $("#newButton").val()
+    topics.push(newButton);
+    makeButtons();
+    // var addButton = $("<button>").text(newButton);
+    // addButton.attr("class", "button");
+    // $("#buttons").append(addButton);
+});
 
 // Event listener for all buttons
-$(document).on("click", ".button", function () {
+$(document).on("click", ".button", function (event) {
+    // Prevents any of the dynamically generated buttons from refreshing the page
+    event.preventDefault();
     // Clearing any GIFs already on the page
     $("#gifs").empty();
     // Getting text of the clicked button to concatenate with queryURL
@@ -19,7 +40,7 @@ $(document).on("click", ".button", function () {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
         var results = response.data;
         // Displaying the rating and gif on the page
